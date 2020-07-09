4cb9ddb8ea20a1258b7dc61ca7c9095836f05fe1
Olly Larkin <olly.larkin@softwire.com>
2020-07-09 10:07:28 +0100

    uncommented line

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -81,7 +81,7 @@ class MyBot : Bot {
         // Give precedence to recent values
         for (i in 0 until upperBound) {
             if (prevGameRev[i].p1 == currentGameRev[i].p1) total += upperBound - i
-//            if (prevGameRev[i].p2 == currentGameRev[i].p2) total += upperBound - i
+            if (prevGameRev[i].p2 == currentGameRev[i].p2) total += upperBound - i
             // If the players draw, this could also be a part of a pattern independent of what was thrown
             if (prevGameRev[i].p1 == prevGameRev[i].p2 && currentGameRev[i].p1 == currentGameRev[i].p2) total += upperBound - i
         }
